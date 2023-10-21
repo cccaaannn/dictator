cefb54e76e8f94e76de1d9d0df5cc5170a492dc1
cccaaannn <can.kurt.aa@gmail.com>
2023-10-21 20:20:02 +0300

    project init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikReact } from "@builder.io/qwik-react/vite";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        qwikVite({
+            csr: true,
+        }),
+        qwikReact()
+    ],
+    resolve: {
+        alias: [{ find: '@', replacement: '/src' }],
+    },
+});
